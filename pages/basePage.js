99e7069e04177daf16e395e4012c413fb79a7dc3
bazilsarosh <bazil@trypallet.com>
2025-08-09 12:40:55 +0530

    Day 3 - Started POM and added Google Search test

--- pages/basePage.js
+++ pages/basePage.js
@@ -1,6 +1,32 @@
 class BasePage {
-    constructor(page) {
-        this.page = page;
+  constructor(page) {
+    this.page = page;
+    this.searchBox = 'textarea[name="q"]';
+  }
+
+  async goto() {
+    // NCR avoids country redirect; helps with stable selectors
+    await this.page.goto('https://www.google.com/ncr');
+    await this.acceptConsentIfPresent();
+    await this.page.waitForSelector(this.searchBox, { state: 'visible' });
+  }
+
+  async acceptConsentIfPresent() {
+    // Try common consent button labels; ignore if not found
+    const labels = ['I agree', 'Accept all', 'Agree to all', 'Accept', 'I accept'];
+    for (const label of labels) {
+      const btn = this.page.getByRole('button', { name: label, exact: false });
+      if (await btn.count()) {
+        await btn.first().click().catch(() => {});
+        break;
+      }
     }
+  }
+
+  async search(term) {
+    await this.page.fill(this.searchBox, term);
+    await this.page.keyboard.press('Enter');
+  }
 }
+
 module.exports = BasePage;
